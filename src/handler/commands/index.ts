8d9a14c23af1570629a726c1093ea73dae9e0a55
Alex Di Maria <alessandro.dimaria3@outlook.it>
2024-06-17 23:22:50 +0200

    feat: load commands

---
+++ src/handler/commands/index.ts
@@ -0,0 +1,18 @@
+import {readdirSync} from 'fs';
+import {logger} from "../../config";
+
+const commands: any = {}
+
+logger.info("Loading commands...")
+
+const readFile = readdirSync("./src/handler/commands")
+    .filter(file => !file.endsWith("index.ts"))
+
+readFile.forEach(file => commands[file] = require(`./${file}`))
+
+logger.info("Commands loaded!")
+logger.debug(commands, "Commands loaded: ")
+
+export {
+    commands
+}
